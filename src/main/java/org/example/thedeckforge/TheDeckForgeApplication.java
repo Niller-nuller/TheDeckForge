13c09b5c99b55097f5f2bd1cb6607e9f0e817efe
niels <u1537530303@gmail.com>
2026-04-30 10:06:52 +0200

    Initial commit

---
+++ src/main/java/org/example/thedeckforge/TheDeckForgeApplication.java
@@ -0,0 +1,13 @@
+package org.example.thedeckforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheDeckForgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheDeckForgeApplication.class, args);
+    }
+
+}

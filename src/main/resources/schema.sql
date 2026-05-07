46a8717d12c9932728195de4c9d3681b1865dd34
Acefullbuster1234 <Gustavzeus14@gmail.com>
2026-05-07 13:04:05 +0200

    master race bitch

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,55 +1,58 @@
-DROP TABLE IF EXISTS User_Collection_Cards;
-DROP TABLE IF EXISTS User_Collection;
-DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS DeckCards;
+DROP TABLE IF EXISTS Collection;
+DROP TABLE IF EXISTS Deck;
+DROP TABLE IF EXISTS Card;
+DROP TABLE IF EXISTS User;
 DROP TABLE IF EXISTS Credentials;
-DROP TABLE IF EXISTS Card_Register;
 
-CREATE TABLE Card_Register (
-    CardId BIGINT AUTO_INCREMENT PRIMARY KEY,
-    CharacterName VARCHAR(100),
-    CardType VARCHAR(50),
-    Color VARCHAR(50),
-    CardSet VARCHAR(100),
-    Rarity VARCHAR(50),
-    RuleText VARCHAR(1800),
-    PictureReference VARCHAR(500),
-    ManaCost VARCHAR(20),
-    ATK INT,
-    DEF INT NOT NULL
-);
 
 CREATE TABLE Credentials (
-    CredentialsId BIGINT AUTO_INCREMENT PRIMARY KEY,
-    Email VARCHAR(100) NOT NULL UNIQUE,
-    PasswordHash VARCHAR(255) NOT NULL,
-    UserRole VARCHAR(50) NOT NULL DEFAULT 'MEMBER'
+                             CredentialsId BIGINT AUTO_INCREMENT PRIMARY KEY,
+                             Email VARCHAR(100) NOT NULL UNIQUE,
+                             PasswordHash VARCHAR(255) NOT NULL,
+                             UserRole VARCHAR(50) NOT NULL DEFAULT 'MEMBER'
 );
 
-CREATE TABLE Users (
-    UserId BIGINT AUTO_INCREMENT PRIMARY KEY,
-    Name VARCHAR(100),
-    Age INT NOT NULL,
-    UserCredentialsId BIGINT NOT NULL,
-    FOREIGN KEY (UserCredentialsId) REFERENCES Credentials(CredentialsId)
+create table Users (
+                      UserId BIGINT AUTO_INCREMENT primary key,
+                      Name varchar(100),
+                      Age date not null,
+                      UserCredentialsId BIGINT NOT NULL unique,
+                      FOREIGN KEY (UserCredentialsId) REFERENCES Credentials(CredentialsId)
+);
+create table Collections(
+                           collectionId BIGINT auto_increment primary key,
+                           UserId bigint not null,
+                           CardId BIGINT Not null,
+                           foreign key (UserId) references User(UserId),
+                           foreign key (CardId) references Card (CardId)
+);
+create table Cards (
+                      CardId BIGINT AUTO_INCREMENT PRIMARY KEY,
+                      CharacterName VARCHAR(100),
+                      CardType VARCHAR(50),
+                      Color VARCHAR(50),
+                      CardSet VARCHAR(100),
+                      Rarity VARCHAR(50),
+                      RuleText VARCHAR(1800),
+                      PictureReference VARCHAR(500),
+                      ManaCost VARCHAR(20),
+                      ATK INT,
+                      DEF INT NOT NULL
 );
 
-CREATE TABLE User_Collection (
-    UserCollectionId BIGINT Auto_increment primary key,
-    UserId BIGINT NOT NULL,
-    CollectionVisibility VARCHAR(20) NOT NULL,
-    FOREIGN KEY (UserId) REFERENCES Users(UserId)
+create table Decks (
+                      DeckName varchar(255) not null,
+                      Format Varchar(50),
+                      DeckId BIGINT auto_increment primary key,
+                      UserId BIGINT not null,
+                      foreign key (UserId) references User(UserId)
 );
 
-CREATE TABLE User_Collection_Cards (
-    UserCollectionId BIGINT not null,
-    CardId BIGINT NOT NULL,
-    Quantity INT NOT NULL DEFAULT 1,
-    CollectionCardStatus VARCHAR(20) NOT NULL,
-    primary key (UserCollectionId),
-    foreign key (UserCollectionId) references User_Collection(UserCollectionId),
-    FOREIGN KEY (CardId) REFERENCES Card_Register(CardId)
+create table DeckCards(
+                          DeckCardId bigint primary key,
+                          DeckId bigint,
+                          CardId bigInt,
+                          foreign key(DeckId) references Deck(DeckId),
+                          foreign key(CardId) references CardId(CardId)
 );
-
-
-
-
